fdd0bb2a9f84ddbf2b0db9af50917f50627ac23c
MachineSpeaker <unknowncoderbase@gmail.com>
2023-02-24 12:28:50 +0530

    HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;  
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World");
+            // Console.Beep();
+            //const int num=0;
+            // decimal m = 1.6m  ;
+            // checked
+            // {
+            // byte a = 255;
+            // a++;
+
+            // Console.WriteLine(a);
+            // }
+            // {
+            //     int q = 5;
+            // }
+            // string strMeowWow = "";
+            int m=9;
+            {
+                int a = 5;
+                {
+                    int q = 3;
+                    {
+                        int c = 9;
+                    //Console.WriteLine(m);
+                    }
+                //Console.WriteLine(c);
+
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
